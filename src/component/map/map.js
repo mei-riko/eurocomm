8d1e10e50dc42c7318488c22219bfffee6b040a5
mei-riko <dobrovolska.marina@gmail.com>
2021-01-28 10:15:41 +0300

    contact page

--- src/component/map/map.js
+++ src/component/map/map.js
@@ -4,219 +4,268 @@ $(document).ready(() =>{
     // MapBox
     mapboxgl.accessToken = 'pk.eyJ1Ijoid2Vic29obyIsImEiOiJja2d0YnJleGMwOWs4MnJvNjZmeHQ0ZHd5In0.O3SaifC5NcA4dP8eWN1O1w';
 
-    var map = new mapboxgl.Map({
-        container: 'map',
+    if( $("#map").length > 0 ){
+      var map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/light-v10',
+          center: [12, 30],
+          zoom: 1
+      });
+
+      var nav = new mapboxgl.NavigationControl();
+      map.addControl(nav, 'top-left');
+
+      // Points
+      var geojsonOrigin = {
+          type: 'FeatureCollection',
+          features: [{
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [25.286389,42.725351]
+            },
+            properties: {
+              title: 'Bulgaria',
+              description: 'Origins'
+            }
+          },
+          {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [24.744553,46.192021]
+              },
+              properties: {
+                title: 'Romania',
+                description: 'Origins'
+              }
+          },
+          {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [25.8441,56.896888]
+              },
+              properties: {
+                title: 'Latvia',
+                description: 'Baltic Sea countries | Origins'
+              }
+          },
+          {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [25.660789,58.564388]
+              },
+              properties: {
+                title: 'Estonia',
+                description: 'Baltic Sea countries | Origins'
+              }
+          },
+          {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [23.600189,55.508473]
+              },
+              properties: {
+                title: 'Lithuania',
+                description: 'Baltic Sea countries | Origins'
+              }
+          },
+          {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [99.505405,61.698653]
+              },
+              properties: {
+                title: 'Russia',
+                description: 'Origins'
+              }
+          },
+          {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [31.482771,49.02753]
+              },
+              properties: {
+                title: 'Ukraine',
+                description: 'Origins'
+              }
+          },
+          {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [-99.115868,36.952915]
+              },
+              properties: {
+                title: 'North America',
+                description: 'Origins'
+              }
+          },
+          {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [-53.373061,-8.626851]
+              },
+              properties: {
+                title: 'South America',
+                description: 'Origins'
+              }
+          }]
+      };
+      var geojsonDest = {
+          type: 'FeatureCollection',
+          features: [{
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [-7.309735,30.863677]
+            },
+            properties: {
+              title: 'Morocco',
+              description: 'Destinations'
+            }
+          },
+          {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [9.602442,34.773985]
+              },
+              properties: {
+                title: 'Tunis',
+                description: 'Destinations'
+              }
+          },
+          {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [17.468477,27.747254]
+              },
+              properties: {
+                title: 'Libya',
+                description: 'Destinations'
+              }
+          },
+          {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [29.90523,26.112189]
+              },
+              properties: {
+                title: 'Egypt',
+                description: 'Destinations'
+              }
+          },
+          {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [56.30792,20.114063]
+              },
+              properties: {
+                title: 'Oman',
+                description: 'Destinations'
+              }
+          },
+          {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [45.725533,22.848295]
+              },
+              properties: {
+                title: 'Saudi Arabia',
+                description: 'Destinations'
+              }
+          }]
+      };
+      // Add markers to map
+      geojsonOrigin.features.forEach(function(markerOrig) {
+          // create a HTML element for each feature
+          var elOrig = document.createElement('div');
+          elOrig.className = 'map-marker-1';
+
+          // make a marker for each feature and add to the map
+          new mapboxgl.Marker(elOrig)
+              .setLngLat(markerOrig.geometry.coordinates)
+              .addTo(map);
+          // popup
+          new mapboxgl.Marker(elOrig)
+              .setLngLat(markerOrig.geometry.coordinates)
+              .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+                  .setHTML('<p>' + markerOrig.properties.title + '</p><p class="text-small mb-0">' + markerOrig.properties.description + '</p>'))
+              .addTo(map);
+      });
+      geojsonDest.features.forEach(function(markerDest) {
+          // create a HTML element for each feature
+          var elDest = document.createElement('div');
+          elDest.className = 'map-marker-2';
+
+          // make a marker for each feature and add to the map
+          new mapboxgl.Marker(elDest)
+              .setLngLat(markerDest.geometry.coordinates)
+              .addTo(map);
+          // popup
+          new mapboxgl.Marker(elDest)
+              .setLngLat(markerDest.geometry.coordinates)
+              .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+                  .setHTML('<p>' + markerDest.properties.title + '</p><p class="text-small mb-0">' + markerDest.properties.description + '</p>'))
+              .addTo(map);
+      });
+    }
+
+    // For Contact Page
+    if( $("#mapOffice").length > 0 ){
+      var mapOffice = new mapboxgl.Map({
+        container: 'mapOffice',
         style: 'mapbox://styles/mapbox/light-v10',
-        center: [12, 30],
-        zoom: 1
-    });
+        center: [6.1493, 46.200767],
+        zoom: 14
+      });
+  
+      var navOffice = new mapboxgl.NavigationControl();
+      mapOffice.addControl(navOffice, 'top-left');
 
-    var nav = new mapboxgl.NavigationControl();
-    map.addControl(nav, 'top-left');
-
-    // Points
-    var geojsonOrigin = {
+      // Points
+      var geojsonOffice = {
         type: 'FeatureCollection',
         features: [{
           type: 'Feature',
           geometry: {
             type: 'Point',
-            coordinates: [25.286389,42.725351]
+            coordinates: [6.1493, 46.200767]
           },
           properties: {
-            title: 'Bulgaria',
-            description: 'Origins'
+            title: 'Head Office',
+            description: 'EURO COMM SA, 4 Place du Bourg de Four, 1204 Geneva, Switzerland'
           }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [24.744553,46.192021]
-            },
-            properties: {
-              title: 'Romania',
-              description: 'Origins'
-            }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [25.8441,56.896888]
-            },
-            properties: {
-              title: 'Latvia',
-              description: 'Baltic Sea countries | Origins'
-            }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [25.660789,58.564388]
-            },
-            properties: {
-              title: 'Estonia',
-              description: 'Baltic Sea countries | Origins'
-            }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [23.600189,55.508473]
-            },
-            properties: {
-              title: 'Lithuania',
-              description: 'Baltic Sea countries | Origins'
-            }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [99.505405,61.698653]
-            },
-            properties: {
-              title: 'Russia',
-              description: 'Origins'
-            }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [31.482771,49.02753]
-            },
-            properties: {
-              title: 'Ukraine',
-              description: 'Origins'
-            }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [-99.115868,36.952915]
-            },
-            properties: {
-              title: 'North America',
-              description: 'Origins'
-            }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [-53.373061,-8.626851]
-            },
-            properties: {
-              title: 'South America',
-              description: 'Origins'
-            }
         }]
-    };
-    var geojsonDest = {
-        type: 'FeatureCollection',
-        features: [{
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [-7.309735,30.863677]
-          },
-          properties: {
-            title: 'Morocco',
-            description: 'Destinations'
-          }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [9.602442,34.773985]
-            },
-            properties: {
-              title: 'Tunis',
-              description: 'Destinations'
-            }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [17.468477,27.747254]
-            },
-            properties: {
-              title: 'Libya',
-              description: 'Destinations'
-            }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [29.90523,26.112189]
-            },
-            properties: {
-              title: 'Egypt',
-              description: 'Destinations'
-            }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [56.30792,20.114063]
-            },
-            properties: {
-              title: 'Oman',
-              description: 'Destinations'
-            }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [45.725533,22.848295]
-            },
-            properties: {
-              title: 'Saudi Arabia',
-              description: 'Destinations'
-            }
-        }]
-    };
-    // Add markers to map
-    geojsonOrigin.features.forEach(function(markerOrig) {
+      };
+      // Add markers to map
+      geojsonOffice.features.forEach(function(markerOffice) {
         // create a HTML element for each feature
-        var elOrig = document.createElement('div');
-        elOrig.className = 'map-marker-1';
+        var elOffice = document.createElement('div');
+        elOffice.className = 'map-marker-1';
 
         // make a marker for each feature and add to the map
-        new mapboxgl.Marker(elOrig)
-            .setLngLat(markerOrig.geometry.coordinates)
-            .addTo(map);
+        new mapboxgl.Marker(elOffice)
+            .setLngLat(markerOffice.geometry.coordinates)
+            .addTo(mapOffice);
         // popup
-        new mapboxgl.Marker(elOrig)
-            .setLngLat(markerOrig.geometry.coordinates)
+        new mapboxgl.Marker(elOffice)
+            .setLngLat(markerOffice.geometry.coordinates)
             .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-                .setHTML('<p>' + markerOrig.properties.title + '</p><p class="text-small mb-0">' + markerOrig.properties.description + '</p>'))
-            .addTo(map);
-    });
-    geojsonDest.features.forEach(function(markerDest) {
-        // create a HTML element for each feature
-        var elDest = document.createElement('div');
-        elDest.className = 'map-marker-2';
+                .setHTML('<p class="text-bold mb-0">' + markerOffice.properties.title + '</p><p class="text-small mb-0">' + markerOffice.properties.description + '</p>'))
+            .addTo(mapOffice);
+      });
+    }
 
-        // make a marker for each feature and add to the map
-        new mapboxgl.Marker(elDest)
-            .setLngLat(markerDest.geometry.coordinates)
-            .addTo(map);
-        // popup
-        new mapboxgl.Marker(elDest)
-            .setLngLat(markerDest.geometry.coordinates)
-            .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-                .setHTML('<p>' + markerDest.properties.title + '</p><p class="text-small mb-0">' + markerDest.properties.description + '</p>'))
-            .addTo(map);
-    });
 });
